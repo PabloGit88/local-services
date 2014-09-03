18f187dd8056b3a22f0cc46d7d2047ac3b079568
Diego <songecko@gmail.com>
2014-09-03 13:00:45 -0300

    auto resize

--- js/signup.js
+++ js/signup.js
@@ -1,5 +1,12 @@
-function goTop()
+function popupLoaded()
 {
+	var magnificPopup = $.magnificPopup.instance;
+	
+	//Resize the popup
+	var popupHeight = magnificPopup.content.find('iframe').contents().find('html').css('height');
+	$('.mfp-content').animate({ height: popupHeight }, 'slow');
+	
+	//Scroll to top
 	$('html, body').animate({ scrollTop: 0 }, 'slow');
 };
 
